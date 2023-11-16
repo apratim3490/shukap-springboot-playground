4ab7660bc4f11f9297e4e3ba2728fab73fe914a7
apratim shukla <apratim3490@gmail.com>
2023-11-16 12:00:07 +0000

    fix project structure

--- src/main/java/com/shukap/application/SpringBootPlaygroundApplication.java
+++ src/main/java/com/shukap/application/SpringBootPlaygroundApplication.java
@@ -1,4 +1,4 @@
-package com.shukap.playground;
+package com.shukap.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
